427a964a81710645fb61999b49fd0e24645c881e
sverrirgeir <sverrirgeir@gmail.com>
2018-09-04 11:01:35 +0000

    Algorithm written for sequence

---
+++ sequence.py
@@ -0,0 +1,10 @@
+n = int(input("Enter the length of the sequence: ")) # Do not change this line
+tala1=1
+tala2=2
+tala3=3
+count=4
+summa=0
+while n>= count:
+    count =count + 1
+    summa = tala1 + tala2 + tala3
+    print(summa)
