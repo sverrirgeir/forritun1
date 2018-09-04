a38263b210cfc058dcffced44fa8da15717aeca6
sverrirgeir <sverrirgeir@gmail.com>
2018-09-04 11:21:09 +0000

    Algorithm written for sequence

--- sequence.py
+++ sequence.py
@@ -4,7 +4,20 @@ tala2=2
 tala3=3
 count=4
 summa=0
+
+if n ==1:
+    print(1)
+elif n ==2:
+    print(2)  
+else: 
+    print("1")
+    print("2")
+    print("3")      
+
 while n>= count:
     count =count + 1
     summa = tala1 + tala2 + tala3
     print(summa)
+    tala1 = tala2
+    tala2 = tala3
+    tala3 = summa
